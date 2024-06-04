e5c54d8b8dd7f630b35937d51c9d28eee1233ad9
Garbi93 <iseung2093@gmail.com>
2024-06-04 18:17:31 +0900

    06/04 스프링 시큐리티, JWT - 초기 설정

---
+++ src/main/java/com/example/testsecurityjwt20240604/TestSecurityJwt20240604Application.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityjwt20240604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityJwt20240604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityJwt20240604Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsecurityjwt20240604/TestSecurityJwt20240604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityjwt20240604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityJwt20240604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
